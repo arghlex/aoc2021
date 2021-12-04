079296cf4fe1bd701b79c9653f8b9cd68d786960
Alex Mellor <alex.mellor@gmail.com>
2021-12-04 13:13:06 +0000

    day 3 part 2 - needs refactoring, a bit messy

--- day-3.js
+++ day-3.js
@@ -7,7 +7,7 @@ fs.readFile('./input/day3.txt', 'utf8', (err, data) => {
 
     let d = data.split('\n');
     
-    console.log(part1(d));
+    console.log(part2(d));
 
 });
 
@@ -46,4 +46,96 @@ function part1 (data) {
     const powerConsumption = gammaRate * epsilonRate;
     
     return powerConsumption;
+}
+
+// ----------------------------
+
+function getOccurence(data, type) {
+
+    const total = data.length;
+    const items = Array(data[0].length).fill(0);
+    
+    for (let i = 0; i < total; i++) {
+        
+        const str = data[i];
+        
+        for (let j = 0; j < str.length; j++) {
+            const element = str[j];
+            if (element === '1') items[j]++
+        }        
+    }
+    
+    const occurence = items.map(x => {
+        let y = total - x;
+        if (type === 'common') {
+            if ( x >= y ) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (type === 'uncommon') {
+            if ( x >= y ) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    });
+
+    return occurence;
+}
+
+
+function part2 (data) {
+
+    const lifeSupportRating = getO2GenRating(data) * getCO2ScrubRating(data);
+
+    return lifeSupportRating;
+
+}
+
+function getO2GenRating (data) {
+    let tmp = data;
+    let count = 0;
+
+    while (tmp.length > 1) {
+        
+        let mc = getOccurence(tmp, 'common');
+        // console.log(mc);
+        let mcCurrent = mc[count];
+
+        tmp = tmp.filter(val => {
+
+            const myStr = val.charAt(count);
+
+            return (parseInt(myStr, 10) === mcCurrent);
+        });
+        // console.log(tmp);
+        count++;
+    }
+
+    return (parseInt(tmp[0], 2));
+}
+
+function getCO2ScrubRating (data) {
+    let tmp = data;
+    let count = 0;
+
+    while (tmp.length > 1) {
+        
+        let mc = getOccurence(tmp, 'uncommon');
+        // console.log(mc);
+        let mcCurrent = mc[count];
+
+        tmp = tmp.filter(val => {
+
+            const myStr = val.charAt(count);
+
+            return (parseInt(myStr, 10) === mcCurrent);
+        });
+        // console.log(tmp);
+        count++;
+    }
+    return (parseInt(tmp[0], 2));
 }
